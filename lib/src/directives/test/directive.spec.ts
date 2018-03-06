e257fe57b36639ca443041248b111b47ab8ecaa1
Brian B <15702512+briancodes@users.noreply.github.com>
2018-03-06 21:23:23 +0100

    Test configuration - not the actual test, but the setup

---
+++ lib/src/directives/test/directive.spec.ts
@@ -0,0 +1,10 @@
+import { TestBed, async } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { RouterLinkDelayModule } from '@bcodes/ngx-routerlink-delay';
+
+describe('Library', () => {
+    it('tests should be reachable', async(() => {
+        const a = {};
+        expect(a).toBeTruthy();
+    }));
+});
--- lib/src/index.ts
+++ lib/src/index.ts
@@ -1,3 +1,4 @@
+// Only files reachable via these exports are included in the bundled library
 export * from './directives/router-link-delay';
 export * from './directives/router-link-active-delay';
 export * from './router-link-delay.module';
