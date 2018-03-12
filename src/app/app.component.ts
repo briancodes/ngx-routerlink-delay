e7ef81b3affba141ecfb6fccc8f4e676c11212d2
Brian B <15702512+briancodes@users.noreply.github.com>
2018-03-12 14:02:44 +0100

    Demo app (#5)
    * Demo App Commit
    
    * Navigation delay update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,26 @@
 import { Component } from '@angular/core';
 
 @Component({
-  // tslint:disable-next-line:component-selector
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    // tslint:disable-next-line:component-selector
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+    title = 'ngx-routerlinkdelay';
+    isMenuOpen = false;
+    isDelay = true;
+
+    menuButtonClick(event: MouseEvent) {
+        this.isMenuOpen = !this.isMenuOpen;
+    }
+
+    onLinkClick(event: MouseEvent) {
+        this.isMenuOpen = false;
+    }
+
+    onCheckboxChange(event: Event) {
+        const value = event.target['checked'];
+        this.isDelay = value;
+    }
 }
--- src/app/pages/page-two/page-two.component.ts
+++ src/app/pages/page-two/page-two.component.ts
@@ -1,19 +1,44 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'bc-page-two',
-  template: `
-    <p>
-      page-two works!
-    </p>
+    selector: 'bc-page-two',
+    template: `
+    <div class="demo-page page-two">
+        <h1>Page Two</h1>
+    </div>
   `,
-  styles: []
+    styles: [`
+       .page-two {
+           position: absolute;
+           display: flex;
+           align-items: center;
+           justify-content: center;
+           bottom: 10%;
+           top: 10%;
+           right: 10%;
+           left: 10%;
+           border-radius: 10px;
+           background-color: #d96666;
+           background-size: cover;
+           background-position: top center;
+           background-image: url('/assets/pexels-photo-440731.jpeg');
+        }
+        h1 {
+            text-transform: uppercase;
+            font-size: 3em;
+            font-weight: bold;
+            background-color: #414141;
+            color: ghostwhite;
+            border-radius: 10px;
+            padding: 0px 15px;
+        }
+    `]
 })
 export class PageTwoComponent implements OnInit {
 
-  constructor() { }
+    constructor() { }
 
-  ngOnInit() {
-  }
+    ngOnInit() {
+    }
 
 }
